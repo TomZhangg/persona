999b602aeb0cdf626d473d22bde81bdf90c885d7
TomZhangg <t.thomaszhang@gmail.com>
2022-10-03 01:09:41 -0400

    s

--- PMP/slit_webapp.py
+++ PMP/slit_webapp.py
@@ -9,24 +9,57 @@ def load_twitterScraper(api_key, api_secrets, access_token, access_secret):
 	return twitterScraper(api_key, api_secrets, access_token, access_secret)
 
 
+#st.title('Persona')
+
+
+
+
 
 scraper = load_twitterScraper(api_key, api_secrets, access_token, access_secret)
+
 if 'twitter_df' not in st.session_state:
 	st.session_state.twitter_df = pd.DataFrame(columns=['name', 'tag', 'followers count', 'idols count'])
 
 
-twitter_search1 = st.text_input(label='@add_twitter_account1')
-if twitter_search1 != '':
-	try:
-		name, screen_name, followers_count, idols_count = scraper.extract_public_traits(twitter_search1)
-		temp_df = pd.DataFrame(data=[[name, screen_name, followers_count, idols_count]], 
-											columns=['name', 'tag', 'followers count', 'idols count'])
-		st.session_state.twitter_df = st.session_state.twitter_df.append(temp_df)
-	except:
-		st.write('invalid')
+display_twitter = st.checkbox('Twitter', value=False)
+
+if display_twitter:
+
+	twitter_search1 = st.text_input(label='@add_twitter_account')
+	if twitter_search1 != '':
+		try:
+			name, screen_name, followers_count, idols_count = scraper.extract_public_traits(twitter_search1)
+			temp_df = pd.DataFrame(data=[[name, screen_name, followers_count, idols_count]], 
+												columns=['name', 'tag', 'followers count', 'idols count'])
+			st.session_state.twitter_df = st.session_state.twitter_df.append(temp_df)
+		except:
+			st.write('invalid')
+
+	if len(st.session_state.twitter_df) > 0:
+		st.session_state.twitter_df = st.session_state.twitter_df.tail(3)
+		st.table(st.session_state.twitter_df.reset_index(drop=True))
+
+else:
+	pass
 
 
-st.table(st.session_state.twitter_df.reset_index(drop=True))
+
+
+display_insta = st.checkbox('Instagram', value=False)
+
+if display_insta:
+	insta_search1 = st.text_input(label='@add_instagram_account')
+
+
+
+
+display_tiktok = st.checkbox('TikTok', value=False)
+
+display_twitch = st.checkbox('Twitch', value=False)
+
+display_youtube= st.checkbox('YouTube', value=False)
+
+
 
 # twitter_search2 = st.text_input(label='@add_twitter_account2')
 # if twitter_search2 != '':
